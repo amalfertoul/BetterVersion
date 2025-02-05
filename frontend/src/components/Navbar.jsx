1350660240fe9f14ea4e7d1357eff98e11caa70e
amalfertoul <142597174+amalfertoul@users.noreply.github.com>
2025-02-05 17:32:28 +0100

    created Navbar component with a drop-down menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,38 @@
-import { Link} from 'react-router-dom';
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons
+import '/styles/navbar.css';
 
 const Navbar = () => {
-    
     return (
         <>
-            <nav>
-
-                <Link to="/" >Home</Link>
-
-                <Link to="/" >Login</Link>
-                <Link to="/" >Sign-up</Link>
-
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">LOGO</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/signup">Sign-up</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="bi bi-list fs-4"></i> {/* Bootstrap Icon for a menu */}
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><Link className="dropdown-item" to="/page1">Page 1</Link></li>
+                                    <li><Link className="dropdown-item" to="/page2">Page 2</Link></li>
+                                    <li><Link className="dropdown-item" to="/page3">Page 3</Link></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </>
     );
