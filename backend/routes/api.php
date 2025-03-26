1db5205a3e301e43ec38a6909b69201a98c877c6
amalfertoul <142597174+amalfertoul@users.noreply.github.com>
2025-03-26 14:17:40 +0000

    update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,40 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\ImageController;
-use App\Http\Controllers\API\CategoryController;
-use App\Http\Controllers\API\TaskController;
-use App\Http\Controllers\API\MessageController;
-use App\Http\Controllers\API\UserController;
-use App\Http\Controllers\API\UserPerformanceController;
-use App\Http\Controllers\API\MiniGameController;
-use App\Http\Controllers\API\FriendRequestController;
-
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserPerformanceController;
+use App\Http\Controllers\MiniGameController;
+use App\Http\Controllers\FriendRequestController;
 
 
 Route::apiResource('images', ImageController::class);
 
-// Routes pour le contrôleur Category
 Route::apiResource('categories', CategoryController::class);
 
-// Routes pour le contrôleur Task
 Route::apiResource('tasks', TaskController::class);
 
-// Routes pour le contrôleur Message
 Route::apiResource('messages', MessageController::class);
 
-// Routes pour le contrôleur User
 Route::apiResource('users', UserController::class);
 
-// Routes pour le contrôleur UserPerformance
 Route::apiResource('user-performance', UserPerformanceController::class);
 
-// Routes pour le contrôleur MiniGame
 Route::apiResource('mini-games', MiniGameController::class);
 
-// Routes pour le contrôleur FriendRequest
 Route::apiResource('friend-requests', FriendRequestController::class);
