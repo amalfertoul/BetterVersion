5fcd2456a1c037b103984f6616ff54d7fa8a820b
yousra_hamdan <youyousstahm26@gmail.com>
2025-04-11 15:05:06 +0100

    adding task_is to vision board migration

--- backend/database/migrations/2025_03_16_103118_create_vision_boards_table.php
+++ backend/database/migrations/2025_03_16_103118_create_vision_boards_table.php
@@ -15,8 +15,14 @@ return new class extends Migration
             $table->id('board_id');
             $table->string('name');
             $table->boolean('visibility')->default(true); // assuming default visibility is true
+
+            //user_id is the foreign key referencing the users table
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            //task_id is the foreign key referencing the tasks table
+            $table->unsignedBigInteger('task_id')->nullable();
+            $table->foreign('task_id')->references('task_id')->on('tasks')->onDelete('cascade');
             $table->timestamps();
         });
     }
