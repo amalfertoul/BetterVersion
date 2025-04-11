bb54c63277a36481fad2a1321cf69af8ac305929
yousra_hamdan <youyousstahm26@gmail.com>
2025-04-11 16:16:34 +0100

    fresh new seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -8,13 +8,11 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
         $this->call([
             QuotesTableSeeder::class,
         ]);
-        $this->call([
-            AdminSeeder::class,
-        ]);
+    
         
     }
 }
