a04afd498e12f8c5cc9816c9db48ca6c750a59fc
khadija-benkhadda <khadijabenkadda@gmail.com>
2025-04-14 16:58:42 +0200

    users slice done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import MoodboardPage from "./containers/MoodBoardPage";
-import ImageGallery from "./containers/ImageGallery";
-import "./styles/app.css";
-import DownLoadButton from './components/DownLoadButton';
-import PinIt from './components/PinIt';
+import React from 'react';
+import UserApiTester from './UserApiTester';
 
-import TasksPage from "./containers/TasksPage";
-import StarButton from "./components/StarButton";
-import Bookmark from "./components/Bookmark";
-
-
-import UsersList from "./apiTests_coponents/UsersList";
-
-
-const App = () => {
-
+function App() {
   return (
-    <Provider store={store}>
-      <Router>   
-        {/* <Navbar/>
-        <StarButton>Click Me</StarButton>
-        <Bookmark />
-        <DownLoadButton /> 
-        <PinIt/> */}
-        <Routes>
-          <Route path="/" element={<TasksPage />} />
-          <Route path="/gallery" element={<ImageGallery />} />
-          <Route path="/moodboard/:taskId" element={<MoodboardPage />} /> 
-          <Route path="/userslist" element={<UsersList />} />
-
-        </Routes>
-
-      </Router>
-      
-    </Provider>
+    <div className="App">
+      <UserApiTester />
+    </div>
   );
-};
+}
 
 export default App;
