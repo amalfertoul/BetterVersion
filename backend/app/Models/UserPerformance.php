0c940e9556a404c25094c97be7705219cf177552
yousra_hamdan <youyousstahm26@gmail.com>
2025-04-15 11:17:45 +0100

    fixing imports

--- backend/app/Models/UserPerformance.php
+++ backend/app/Models/UserPerformance.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Task; // Import the Task model
 
 class UserPerformance extends Model
 {
