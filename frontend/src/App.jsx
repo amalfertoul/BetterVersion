e5fcce707575a0b39d4a1e94de5497c05163bfa2
yousra_hamdan <youyousstahm26@gmail.com>
2025-04-30 17:13:18 +0100

    register worked yey

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Quotes from './sections/Quotes'; // Assuming Quotes is in the same directory
+import Register from './components/register';
 function App() {
   return (
     <div>
       <Quotes />
+      <Register />
+      {/* Other components can be added here */}
     </div>
   );
 }
