ef192cbb8f924d525b758b09144be27841e3e2d3
amalfertoul <142597174+amalfertoul@users.noreply.github.com>
2025-04-30 20:42:38 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store'; 
 
-ReactDOM.render(
-  <React.StrictMode>
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
